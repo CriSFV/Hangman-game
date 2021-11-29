fe21d0e1a4370c3669b5b7d01358039ed592c098
CriSFV <cristina.f.valverde@gmail.com>
2021-11-29 10:33:41 +0100

    add form component

--- src/components/App.js
+++ src/components/App.js
@@ -6,12 +6,14 @@ import Header from './Header';
 import Dummy from './Dummy';
 import SolutionLetters from './SoluctionLetters';
 import ErrorLetters from './ErrorLetters';
+import Form from './Form';
 
 function App() {
   const [userLetter, setUserLetter] = useState([]); //donde se almacena las letras de la jugadora (todas)
   const [lastLetter, setLastLetter] = useState(''); //string para almacenar al última letra introducida por la jugadora (si no es permitida no se incluye, por eso no lo pinta aunque marques tecla)
   const [word, setWord] = useState(''); //donde) se va a almacenar la palabra a adivinar
 
+  //petición random word:
   useEffect(() => {
     callToApi().then((response) => {
       setWord(response);
@@ -19,11 +21,10 @@ function App() {
   }, []);
 
   const handlerLetter = (ev) => {
-    const inputValue = ev.target.value;
     let regex = new RegExp('^[a-zA-Z]$'); // letras permitidas, el resto no lo están
-    console.log(inputValue);
-    if (inputValue.match(regex)) {
-      setUserLetter([...userLetter, inputValue]);
+    // si la letra esta permitida:
+    if (ev.match(regex)) {
+      setUserLetter([...userLetter, ev]);
     } else {
       setLastLetter(''); //si es número no pinta nada porque aquí le estamos diciendo que sea string vacío y tampoco se guarda en el estado
     }
@@ -44,22 +45,7 @@ function App() {
         <section>
           <SolutionLetters word={word} userLetter={userLetter} />
           <ErrorLetters word={word} userLetter={userLetter} />
-
-          <form className='form'>
-            <label className='title' htmlFor='last-letter'>
-              Escribe una letra:
-            </label>
-            <input
-              autoComplete='off'
-              className='form__input'
-              maxLength='1'
-              type='text'
-              name='last-letter'
-              id='last-letter'
-              value={lastLetter}
-              onChange={handlerLetter}
-            />
-          </form>
+          <Form handlerLetter={handlerLetter} lastLetter={lastLetter} />
         </section>
         <Dummy numberError={numberError()} />
       </main>
